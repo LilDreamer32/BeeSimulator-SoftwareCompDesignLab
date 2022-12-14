1e1054619bc92d3ff75fe453088153a03611bef4
furste <furste@msoe.edu>
2022-12-14 11:45:23 -0600

    add empty domain classes

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("BeeSimulator.fxml"));
-        primaryStage.setTitle("Barry Bee Benson");
+        primaryStage.setTitle("Barry src.Bee Benson");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
